e5aa38cdcf80e1027b4de457481a8db4805d2377
NeilDG <NeilDG@github.com>
2018-01-13 13:29:31 +0800

    Finished demo application.

--- app/src/main/java/com/neildg/androiddemoone/MainActivity.java
+++ app/src/main/java/com/neildg/androiddemoone/MainActivity.java
@@ -4,13 +4,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements TextWatcher {
     private final static String TAG = "MainActivity";
+
+    private final String displayFormat = "Hello %s! Welcome to your first mobile application!";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,12 +26,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setupBehavior() {
-        final String displayFormat = "Hello %s! Welcome to your first mobile application!";
+
 
         //get all layout references
         final EditText nameText = (EditText) this.findViewById(R.id.name_text);
         final TextView msgView = (TextView) this.findViewById(R.id.display_msg_view);
-        Button submitBtn = (Button) this.findViewById(R.id.submit_btn);
+        final Button submitBtn = (Button) this.findViewById(R.id.submit_btn);
+        CheckBox refreshCheck = (CheckBox) this.findViewById(R.id.refresh_checkbox);
 
         //add button listener
         submitBtn.setOnClickListener(new View.OnClickListener() {
@@ -37,23 +43,38 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        //on the fly listener
-        nameText.addTextChangedListener(new TextWatcher() {
+        refreshCheck.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                msgView.setText(String.format(displayFormat, s));
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked) {
+                    //on the fly listener
+                    nameText.addTextChangedListener(MainActivity.this);
+                    submitBtn.setEnabled(false);
+                }
+                else {
+                    nameText.removeTextChangedListener(MainActivity.this);
+                    submitBtn.setEnabled(true);
+                }
             }
         });
 
+
+
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+        final TextView msgView = (TextView) this.findViewById(R.id.display_msg_view);
+        msgView.setText(String.format(displayFormat, s));
     }
 }
